889641c183c3f0a0abae942d5f5a24338e202497
Shubham <shubham@itsbhm.group>
2021-03-18 19:58:09 +0530

    React State and Basic

--- Module 2 - Base Features & Syntax/react-guide/src/App.js
+++ Module 2 - Base Features & Syntax/react-guide/src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Mini', age: 18 },
+      { name: 'Sweety', age: 22 },
+      { name: 'Arshi', age: 20 }
+    ]
+  }
+
+  switchNameHandler = () => {
+
+    console.log("Button Clicked");
+
+    this.setState({
+      persons: [
+        { name: 'Somya', age: 17 },
+        { name: 'Harsh', age: 24 },
+        { name: 'Arshi', age: 19 }
+      ]
+    })
+
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Type: User </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
   }
