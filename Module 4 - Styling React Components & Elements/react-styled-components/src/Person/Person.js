07f6ed159e5369b2d84a0250ca0f2693a1549f96
Shubham <shubham@itsbhm.group>
2021-03-25 17:04:05 +0530

    Styled Components

---
+++ Module 4 - Styling React Components & Elements/react-styled-components/src/Person/Person.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+// import "./Person.css";
+
+const StyledDiv = styled.div`
+  width: 50%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
+
+const person = (props) => {
+  return (
+    <StyledDiv>
+      <p onClick={props.click}>
+        Im a {props.name} and I am {props.age} year old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" value={props.name} onChange={props.changed} />
+      {/* <input type="text" onChange={props.changed} value={props.name} /> */}
+    </StyledDiv>
+  );
+};
+
+export default person;
