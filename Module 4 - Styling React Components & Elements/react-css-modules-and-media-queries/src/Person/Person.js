ee80a4629d83e591b8019aed8fae6842068c20ad
Shubham <shubham@itsbhm.group>
2021-03-26 18:57:36 +0530

    CSS Modules and media queries

--- Module 4 - Styling React Components & Elements/react-css-modules-and-media-queries/src/Person/Person.js
+++ Module 4 - Styling React Components & Elements/react-css-modules-and-media-queries/src/Person/Person.js
@@ -1,23 +1,9 @@
 import React from "react";
-import styled from "styled-components";
-// import classes from "./Person.css";
-
-const StyledDiv = styled.div`
-  width: 50%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
+import classes from "./Person.css";
 
 const person = (props) => {
   return (
-    <div>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         Im a {props.name} and I am {props.age} year old!
       </p>
