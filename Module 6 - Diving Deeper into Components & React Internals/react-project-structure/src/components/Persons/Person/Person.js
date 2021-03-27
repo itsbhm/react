2d6fd2de6f308163645e57991aa6e8fdd3bf856b
Shubham <shubham@itsbhm.group>
2021-03-28 02:21:47 +0530

    Better Project Structure

---
+++ Module 6 - Diving Deeper into Components & React Internals/react-project-structure/src/components/Persons/Person/Person.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./Person.css";
+
+const person = (props) => {
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        Im a {props.name} and I am {props.age} year old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" value={props.name} onChange={props.changed} />
+      {/* <input type="text" onChange={props.changed} value={props.name} /> */}
+    </div>
+  );
+};
+
+export default person;
---
+++ Module 6 - Diving Deeper into Components & React Internals/react-project-structure/src/containers/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+
+import classes from "./App.css";
+import Person from "../components/Persons/Person/Person";
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: "1", name: "Mini", age: 18 },
+      { id: "2", name: "Sweety", age: 22 },
+      { id: "3", name: "Arshi", age: 20 },
+    ],
+    otherState: "Some other value",
+    showPersons: false,
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({
+      persons: persons,
+    });
+  };
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    let persons = null;
+    let btnClass = [classes.Button];
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+      // style.backgroundColor = "red"; // Raising error 'style' not found
+      btnClass.push(classes.Red);
+    }
+
+    // const classes = ["text-red", "bold"].join(" ");
+
+    const assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red); // assignedClasses ['red']
+    }
+
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold); // assignedClasses ['red', 'bold']
+    }
+
+    return (
+      <div className={classes.App}>
+        <h1>Hi, I'm a React App</h1>
+        {/* <button onClick={this.switchNameHandler}>Switch Name</button> */}
+        <p className={assignedClasses.join(" ")}>Development made easy!</p>
+        <button
+          className={btnClass.join(" ")}
+          onClick={this.togglePersonHandler}
+        >
+          Toggle Button
+        </button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
